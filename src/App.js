878c05b05c49510a0cb169bcbf87ef8873b5d804
Adrian Sivira <adriansivira@gmail.com>
2022-08-10 21:16:40 -0300

    actualizacion

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route
-            path={"/card/:nombre"}
+            path={"/card/:name"}
             element={<Card pokemon={pokemon} />}
           ></Route>
         </Routes>
--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -2,49 +2,61 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 export const Card = ({ pokemon }) => {
-  const { nombre } = useParams();
-  // encontrar en la lista de pokemon que este componente recibe como parametro
+  const { name } = useParams();
+  // encontrar en la lista de pokemon, que este componente recibe como parametro)
   // todos los datos del pokemon con el nombre = nombre params
+
+  let selectedPokemon = [];
+
+  pokemon.map((poke) => {
+    if (poke.name === name) {
+      selectedPokemon.push(poke);
+      return selectedPokemon;
+    }
+  });
+
+  const newPokemon = selectedPokemon[0];
+
   return (
     <div className="card">
       <div className="pokeHeader">
-        <h1>{pokemon.name}</h1>
-        <p>#{pokemon.id}</p>
+        <h1>{newPokemon.name}</h1>
+        <p>#{newPokemon.id}</p>
       </div>
-      {/* <div className="pokeImg">
-        <img src={props.img} alt="" />
+      <div className="pokeImg">
+        <img src={newPokemon.img} alt="" />
         <img src="" alt="" />
       </div>
       <div className="types">
-        <p>{props.type}</p>
-        <p>{props.type}</p>
+        <p>{newPokemon.type[0]}</p>
+        <p>{newPokemon.type[1]}</p>
       </div>
       <h2>About</h2>
       <section className="about">
         <div className="weight">
           <div>
             <img src="./Imagenes/Recursos/Weight.svg" />
-            <p>{props.weight}</p>
+            <p>{newPokemon.weight}</p>
           </div>
           <p>Weight</p>
         </div>
         <div className="height">
           <div>
             <img src="./Imagenes/Recursos/Height.svg" />
-            <p>{props.heigth}</p>
+            <p>{newPokemon.heigth}</p>
           </div>
           <p>Height</p>
         </div>
         <div className="moves">
           <div>
-            <p>{props.moves}</p>
-            <p>{props.moves}</p>
+            <p>{newPokemon.moves[0]}</p>
+            <p>{newPokemon.moves[1]}</p>
           </div>
           <p>Moves</p>
         </div>
       </section>
-      <p>{props.description}</p>
-      <h2>Base Stats</h2> */}
+      <p>{newPokemon.description}</p>
+      <h2>Base Stats</h2>
     </div>
   );
 };
