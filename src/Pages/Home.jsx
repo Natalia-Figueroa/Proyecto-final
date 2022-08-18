0d9ab84f513b9a3ba9d544eeff01ea7e6328f2f2
Adrian Sivira <adriansivira@gmail.com>
2022-08-18 20:42:54 -0300

    final commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,71 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../App.css";
 import { PokemonBox } from "../Components/Box/PokemonBox";
 import { pokemonList } from "../Database/Pokemon";
 
 export const Home = () => {
-  const [order, setOrder] = useState(false);
+  const [order, setOrder] = useState(true);
+  const [orderArrow, setorderArrow] = useState(true);
   const [chosenList, setChosenList] = useState(pokemonList);
   const [filter, setFilter] = useState(pokemonList);
 
   let search; //Variable que captura el texto del input
   let filteredList; //Variable que devuelve la lista según la búsqueda
+
+  useEffect(() => {
+    if (order && orderArrow) {
+      const sortingAlphaListMayor = (a, b) => {
+        if (a.name > b.name) {
+          return 1;
+        }
+        if (a.name < b.name) {
+          return -1;
+        }
+      };
+
+      setChosenList([...chosenList].sort(sortingAlphaListMayor));
+      setFilter([...filter].sort(sortingAlphaListMayor));
+    }
+    if (order && !orderArrow) {
+      const sortingAlphaListMinor = (a, b) => {
+        if (a.name < b.name) {
+          return 1;
+        }
+        if (a.name > b.name) {
+          return -1;
+        }
+      };
+
+      setChosenList([...chosenList].sort(sortingAlphaListMinor));
+      setFilter([...filter].sort(sortingAlphaListMinor));
+    }
+    if (!order && orderArrow) {
+      const sortingNumListMayor = (a, b) => {
+        if (a.id > b.id) {
+          return 1;
+        }
+        if (a.id < b.id) {
+          return -1;
+        }
+      };
+
+      setChosenList([...chosenList].sort(sortingNumListMayor));
+      setFilter([...filter].sort(sortingNumListMayor));
+    }
+    if (!order && !orderArrow) {
+      const sortingNumListMinor = (a, b) => {
+        if (a.id < b.id) {
+          return 1;
+        }
+        if (a.id > b.id) {
+          return -1;
+        }
+      };
+
+      setChosenList([...chosenList].sort(sortingNumListMinor));
+      setFilter([...filter].sort(sortingNumListMinor));
+    }
+  }, [order, orderArrow]);
 
   const handleSearch = (e) => {
     search = e.target.value;
@@ -25,32 +81,10 @@ export const Home = () => {
 
   const handleOrder = () => {
     setOrder(!order);
-    if (order) {
-      const sortingAlphaList = (a, b) => {
-        if (a.name > b.name) {
-          return 1;
-        }
-        if (a.name < b.name) {
-          return -1;
-        }
-      };
+  };
 
-      setChosenList([...chosenList].sort(sortingAlphaList));
-      setFilter([...filter].sort(sortingAlphaList));
-    }
-    if (!order) {
-      const sortingNumList = (a, b) => {
-        if (a.id > b.id) {
-          return 1;
-        }
-        if (a.id < b.id) {
-          return -1;
-        }
-      };
-
-      setChosenList([...chosenList].sort(sortingNumList));
-      setFilter([...filter].sort(sortingNumList));
-    }
+  const handleArrowOrder = () => {
+    setorderArrow(!orderArrow);
   };
 
   return (
@@ -58,9 +92,23 @@ export const Home = () => {
       <header>
         <img src="./Imagenes/Recursos/Pokeball.png" />
         <h1>Pokédex</h1>
-        <section className="order arrow" onClick={handleOrder}>
-          {order ? <p>#</p> : <p>Aa</p>}
-          <img src="./Imagenes/Recursos/Arrow.svg" />
+        <section className="order arrow">
+          {order ? (
+            <p onClick={handleOrder}>Aa</p>
+          ) : (
+            <p onClick={handleOrder}>#</p>
+          )}
+          {orderArrow ? (
+            <img
+              onClick={handleArrowOrder}
+              src="./Imagenes/Recursos/down-arrow.png"
+            />
+          ) : (
+            <img
+              onClick={handleArrowOrder}
+              src="./Imagenes/Recursos/up-arrow.png"
+            />
+          )}
         </section>
       </header>
       <input type="search" placeholder="Buscar" onChange={handleSearch} />
